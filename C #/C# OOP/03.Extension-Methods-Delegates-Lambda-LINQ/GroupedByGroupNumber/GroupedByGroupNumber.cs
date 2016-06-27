8656458f14e4e38c88da6d33f90a266daef4b3ef
DragomirPetrov <petrov.dragomir@yahoo.com>
2016-06-27 21:33:58 +0300

    Homework

---
+++ C #/C# OOP/03.Extension-Methods-Delegates-Lambda-LINQ/GroupedByGroupNumber/GroupedByGroupNumber.cs
@@ -0,0 +1,31 @@
+﻿namespace GroupedByGroupNumber
+{
+    using System;
+    using System.Linq;
+
+    public class GroupedByGroupNumber
+    {
+        public static void Main()
+        {
+            var students = new[]
+            {
+                new { Name = "Jorkata", Group = "Informatics"},
+                new { Name = "Kire", Group = "History"},
+                new { Name = "Mara", Group = "Informatics"},
+                new { Name = "Ickata", Group = "Mathematics"},
+                new { Name = "Davidkata", Group = "History"},
+                new { Name = "Igor", Group = "Mathematics"}
+            };
+
+            var groupedStudents = students.GroupBy(s => s.Group).OrderBy(g => g.Key);
+
+            foreach (var group in groupedStudents)
+            {
+                foreach (var student in group)
+                {
+                    Console.WriteLine("{0} -> {1}", student.Group, student.Name);
+                }
+            }
+        }
+    }
+}
---
+++ C #/C# OOP/03.Extension-Methods-Delegates-Lambda-LINQ/OrderStudents/OrderStudents.cs
@@ -0,0 +1,28 @@
+﻿namespace OrderStudents
+{
+    using System;
+    using System.Linq;
+
+    public class OrderStudents
+    {
+        public static void Main()
+        {
+            var arrayOfStudents = new[]
+            {
+                new { FirstName = "Pesho", LastName = "Peshev" },
+                new { FirstName = "Gabriel", LastName = "Batistuta" },
+                new { FirstName = "Bogdan", LastName = "Stanko" },
+                new { FirstName = "Ricardo", LastName = "Sanches" },
+                new { FirstName = "Emanuel", LastName = "Grozev"}
+            };
+
+            var sortedStudents = arrayOfStudents.OrderByDescending(s => s.FirstName).ThenByDescending(s => s.LastName);
+
+            Console.WriteLine("Sorted by descending first name then by descending last name:");
+            foreach (var student in sortedStudents)
+            {
+                Console.WriteLine("{0} {1}", student.FirstName, student.LastName);
+            }
+        }
+    }
+}
