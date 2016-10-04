a639ae3e3d460458fa31c0332bfe90ecdede278a
DragomirPetrov <petrov.dragomir@yahoo.com>
2016-10-04 12:47:27 +0300

    Homework

---
+++ C #/QPC/02.QPC-Part-1/04.Variables-Data-Expressions-and-Constants/02.MethodPrintStatisticsInCSharp/Startup.cs
@@ -0,0 +1,13 @@
+﻿namespace MethodPrintStatisticsInCSharp
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            double[] collection = new double[] { 1.5, 2.5, 3.5, 10.2, 23.4 };
+
+            IPrinter printer = new StatisticsPrinter();
+            printer.PrintStatistics(collection);
+        }
+    }
+}
---
+++ C #/QPC/02.QPC-Part-1/04.Variables-Data-Expressions-and-Constants/02.MethodPrintStatisticsInCSharp/StatisticsPrinter.cs
@@ -0,0 +1,33 @@
+﻿namespace MethodPrintStatisticsInCSharp
+{
+    using System;
+
+    public class StatisticsPrinter : IPrinter
+    {
+        public void PrintStatistics(double[] collection)
+        {
+            double maxValue = double.MinValue;
+            double minValue = double.MaxValue;
+            double sum = 0;
+
+            int collectionLength = collection.Length;
+            for (int i = 0; i < collectionLength; i++)
+            {
+                maxValue = collection[i] > maxValue ? collection[i] : maxValue;
+                minValue = collection[i] < minValue ? collection[i] : minValue;
+                sum += collection[i];
+            }
+
+            PrintNumber("Max value: ", maxValue);
+            PrintNumber("Min value: ", minValue);
+
+            double averageSum = sum / collectionLength;
+            PrintNumber("Average: ", averageSum);
+        }
+
+        public void PrintNumber(string message, double number)
+        {
+            Console.WriteLine($"{message} {number}");
+        }
+    }
+}
